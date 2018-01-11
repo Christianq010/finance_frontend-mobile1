81c196f4133e5460c93c77889d3dc149a67748dd
Christiaan Quyn <christianq010@gmail.com>
2018-01-11 11:45:22 +0530

    Feat: Add id names and class names for pushing content offscreen for navbar
    JS code however commented out due to issues.

--- src/vendor/main.js
+++ src/vendor/main.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "350px";
-    document.getElementById("main").style.marginLeft = "350px";
+    // document.getElementById("main-content").style.marginLeft = "350px";
+    // document.getElementById("navbar-top").style.marginLeft = "350px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    // document.getElementById("main-content").style.marginLeft = "0";
+    // document.getElementById("navbar-top").style.marginLeft = "0";
 }
